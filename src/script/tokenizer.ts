38702f12ed78e9937f72457bd79c3727c9538eb2
steven <github@stevenwaterman.uk>
2020-11-16 16:19:24 +0000

    Simplify regex logic in script transformer

--- src/script/tokenizer.ts
+++ src/script/tokenizer.ts
@@ -20,11 +20,12 @@ export function computeScriptTokens(script: string): ScriptToken[] {
   const {cleaned, replacements} = cleanupScript(script);
 
   const tokens: MarkedToken[] = lexer(cleaned, {gfm: true}) as unknown as MarkedToken[];
+  console.log(JSON.stringify(tokens));
   const paragraphTokens: ParagraphToken[] = tokens.flatMap(toParagraphToken);
   const chunkedTokens: ChunkedToken[] = paragraphTokens.map(token => {
     if(token.text === undefined) return {raw: token.raw, text: []};
 
-    const chunks = token.text.split(/([.?!]+\s+)|(:*\s*\n+)/g);
+    const chunks = token.text.split(/([.?!]+\s+)|\n+/g);
     const definedChunks = chunks.filter(chunk => chunk !== undefined);
     const textChunks = definedChunks.filter((_, idx) => idx % 2 === 0);
     const trimmed = textChunks.map(chunk => chunk.trim());
@@ -79,41 +80,39 @@ function cleanupScript(script: string): {cleaned: string, replacements: Record<s
   return {cleaned, replacements}
 }
 
-function toParagraphToken(token: MarkedToken): ParagraphToken[] {
-  if(token.type === "heading") return [token];
-  if(token.type === "codespan") return [token];
-  if(token.type === "text") return [token];
-  if(token.type === "strong") return [token];
-  if(token.type === "em") return [token];
-  if(token.type === "escape") return [token];
-  if(token.type === "link") return [token];
-  if(token.type === "list_item") return [token];
-  if(token.type === "image") return [token];
+function toParagraphToken(token: MarkedToken): ParagraphToken {
+  if(token.type === "heading") return token;
+  if(token.type === "codespan") return token;
+  if(token.type === "text") return token;
+  if(token.type === "strong") return token;
+  if(token.type === "em") return token;
+  if(token.type === "escape") return token;
+  if(token.type === "link") return token;
+  if(token.type === "list_item") return token;
+  if(token.type === "image") return token;
   
-  if(token.type === "code") return [{raw: token.raw, text: "pause now if you want to look at the code"}];
-  if(token.type === "table") return [{raw: token.raw, text: "pause now if you want to look at the table"}];
+  if(token.type === "code") return {raw: token.raw, text: "pause now if you want to look at the code"};
+  if(token.type === "table") return {raw: token.raw, text: "pause now if you want to look at the table"};
 
   if(token.type === "paragraph") return combineChildren(token);
   if(token.type === "blockquote") return combineChildren(token);
   if(token.type === "list") return combineList(token);
   
-  return [{
+  return {
     raw: token.raw
-  }];
+  };
 }
 
-function combineList(token: Extract<MarkedToken, {type: "list"}>) : ParagraphToken[] {
+function combineList(token: Extract<MarkedToken, {type: "list"}>) : ParagraphToken {
   const raw = token.raw;
-  const children = token.items.flatMap(toParagraphToken);
-  const text = children.map(token => token.text).join("\n");
-  return [{raw, text}];
+  const text = token.items.map(toParagraphToken).map(token => token.text).join("\n");
+  return {raw, text};
 }
 
-function combineChildren(token: Extract<MarkedToken, {tokens: MarkedToken[]}>): ParagraphToken[] {
+function combineChildren(token: Extract<MarkedToken, {tokens: MarkedToken[]}>): ParagraphToken {
   const raw = token.raw;
-  const children = token.tokens.flatMap(toParagraphToken);
-  const text = children.map(token => token.text).join("");
-  return [{raw, text}];
+  const text = token.tokens.map(toParagraphToken).map(token => token.text).join("");
+  return {raw, text};
 }
 
 function toScriptToken(token: ChunkedToken): ScriptToken[] {
